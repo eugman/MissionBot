e5ad07a1c22cf458bf69dbecc516b81758308b4d
Eugene Meidinger <eugene@eugenemeidinger.com>
2023-09-12 15:02:47 -0400

    Added ignore for save file. Updated missions for ITS 15. Added new commands.

--- Mission.py
+++ Mission.py
@@ -24,18 +24,21 @@ import os.path
 import pickle
 
 
-missions = ["Acquisition", "Annihilation", "Biotechvore", "Capture and Protect", 
-        "Countermeasures", "Decapitation", "Firefight", "Frostbyte", 
-        "Frontline", "Highly Classified", "Looting and Sabotage", "Mindwipe", 
-        "Panic Room", "Power Pack", "Quadrant Control", "Rescue", 
+missions = ["Acquisition", "B-pong", "Biotechvore", "Capture and Protect", 
+        "Countermeasures", "Decapitation", "Evacuation", "Firefight",
+        "Frostbyte", "Frontline", "Highly Classified", "Last Launch", 
+        "Looting and Sabotage", "Mindwipe", "Panic Room", "Power Pack", 
         "Supplies", "Supremacy", "The Armory", "Unmasking", 
-        "Non-standard ITS mission", "Narrative Mission"]
+        "Direct Action Mission", "Narrative Mission"]
+
+missionSummary = ["Activate the antennas, control the antennas, control the tech coffin"]
+
 
 narrativeMissions = []
 
 client = discord.Client()
-client.mission = "Unknown mission"
-client.prevMissions = [11, 4, 2, 11]
+client.mission = "Acquisition"
+client.prevMissions = []
 
 if os.path.exists('save.p'):
     client.mission, client.prevMissions = pickle.load(open("save.p", "rb"))
@@ -75,8 +78,24 @@ async def on_message(message):
     if command(message, "previous"):
         await message.channel.send("Previous missions were " + ", ".join([missions[x] for x in client.prevMissions]))
 
+    if command(message, "list"):
+        missionList = ""
+        for i in range(len(missions)):
+            missionList += str(i+1) + " - " + missions[i] + "\n"
+        await message.channel.send("All missions:\n " + missionList)
+
     
+    if command(message, "set"):
+        missionID = int(message.content.split()[-1])-1
+        client.prevMissions = client.prevMissions[1:] + [missionID]
+        client.mission = missions[missionID]
+        await client.change_presence(activity=discord.Game(client.mission))
+
+        pickle.dump([client.mission, client.prevMissions], open("save.p", "wb"))
+        await message.channel.send("You are playing " + client.mission )
+    
+
     if command(message, "help"):
-        await message.channel.send("Commands are\n**!random** will generate a new mission\n**!mission** will show the current mission\n**!previous** will show the previous missions\n**!help** will display this message" )
+        await message.channel.send("Commands are\n**!random** will generate a new mission\n**!mission** will show the current mission\n**!previous** will show the previous missions\n**!list** will show all missions\n**!set** 1 will set it to mission 1\n**!help** will display this message" )
     
 client.run(open("apikey.txt",'r').read().strip())
